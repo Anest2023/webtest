139efce2dd5b6b0fe8d1826e93404bec2552b57d
Anest <efremenko.ira.rzn@gmail.com>
2024-06-27 01:24:49 +0300

    test passed

---
+++ src/test/java/CallbackTest.java
@@ -0,0 +1,60 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CallbackTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    public static void setup() {
+       WebDriverManager.chromedriver().setup();
+   }
+
+    @BeforeEach
+    public void beforeEach() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        //options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+
+    public void shouldBeSuccessfulForm() {
+
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ирина");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+74912242424");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+
+}
+
+
+
+
+
+
+
+
+
